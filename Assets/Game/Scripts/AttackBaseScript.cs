49c6508cf12b5ad68738393cff55053f57beb036
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2025-12-16 10:53:13 +0900

    アタックベースクラス追加

---
+++ Assets/Game/Scripts/AttackBaseScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackBaseScript : MonoBehaviour
+{
+    [SerializeField] float power;
+    [SerializeField] float range;
+    [SerializeField] float interval;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AttackStateMachine()
+    {
+
+    }
+}
