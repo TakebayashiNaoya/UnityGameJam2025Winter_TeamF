3e31176672725e9fe4961767dd5447b92a38d4a9
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2025-12-16 14:08:01 +0900

    キャラクターのシリアライスフィールドの変数にヘッダーコメントを追加

--- Assets/Game/Scripts/Actor/Bases/CharacterBaseScript.cs
+++ Assets/Game/Scripts/Actor/Bases/CharacterBaseScript.cs
@@ -15,22 +15,21 @@ public enum CharacterState
 
 public class CharacterBaseScript : ActorScript
 {
-    [SerializeField] int m_needMoney;
-    [SerializeField] float m_health;
-    [SerializeField] float m_moveSpeed;
-    [SerializeField] float m_spawnInterval;
+    [Header("出撃に必要なお金"), SerializeField] int m_needMoney;
+    [Header("体力"),             SerializeField] float m_health; 
+    [Header("移動速度"),         SerializeField] float m_moveSpeed; 
+    [Header("攻撃力"),           SerializeField] float m_attackPower;
+    [Header("射程"),             SerializeField] float m_range; 
+    [Header("再出撃までの時間"), SerializeField] float m_spawnInterval;  
+    [Header("再攻撃までの時間"), SerializeField] float m_attackInterval; 
+    [Header("攻撃にかかる時間"), SerializeField] float m_attackingTime;  
 
-    [SerializeField] float m_attackPower;
-    [SerializeField] float m_range;
-    [SerializeField] float m_attackInterval;
-    [SerializeField] float m_attackingTime;
+    private float m_standingTimer = 0.0f;       //待機時間計測用タイマー
+    private float m_attackingTimer = 0.0f;      //攻撃時間計測用タイマー
+    private bool m_canAttack = true;            //攻撃可能かどうか
+    private bool m_isDie = false;               //死亡しているかどうか
 
-    private float m_standingTimer = 0.0f;
-    private float m_attackingTimer = 0.0f;
-    private bool m_canAttack = true;
-    private bool m_isDie = false;
-
-    CharacterState currentState = CharacterState.None;
+    CharacterState m_currentState = CharacterState.None;
 
     // Start is called before the first frame update
     void Start()
@@ -45,7 +44,7 @@ public class CharacterBaseScript : ActorScript
 
     protected override void InitializeObject()
     {
-        currentState = CharacterState.Walk;
+        m_currentState = CharacterState.Walk;
     }
 
 
@@ -57,7 +56,7 @@ public class CharacterBaseScript : ActorScript
 
     void CharacterStateMachine()
     {
-        switch (currentState)
+        switch (m_currentState)
         {
             case CharacterState.Idle:
                 IdleStateUpdate();
@@ -65,23 +64,29 @@ public class CharacterBaseScript : ActorScript
                 break;
             case CharacterState.Walk:
                 WalkStateUpdate();
+                WalkChangeState();
                 break;
             case CharacterState.Attack:
+                AttackStateUpdate();
+                AttackChangeState();
                 break;
             case CharacterState.Die:
+                DieStateUpdate();
+                DieChangeState();
                 break;
             default:
                 break;
         }
     }
 
-
+    //アイドルステートの更新処理
     void IdleStateUpdate()
     {
         m_standingTimer += Time.deltaTime;
-        
     }
 
+
+    //アイドルステートの状態遷移処理
     void IdleChangeState()
     {
         if (m_standingTimer >= m_attackInterval)
@@ -91,28 +96,50 @@ public class CharacterBaseScript : ActorScript
         }
     }
 
+
+
+    //歩きステートの更新処理
     void WalkStateUpdate()
     {
         transform.Translate(Vector3.right * m_moveSpeed * Time.deltaTime);
     }
 
+
+    //歩きステートの状態遷移処理
     void WalkChangeState()
     {
 
     }
 
 
+    //攻撃ステートの更新処理
     void AttackStateUpdate()
     {
         m_attackingTimer += Time.deltaTime;
     }
 
 
+    //攻撃ステートの状態遷移処理
     void AttackChangeState()
     {
         if (m_attackingTimer >= m_attackingTime)
         {
-
+            m_currentState = CharacterState.Walk;
+            m_attackingTimer = 0.0f;
         }
+    }
+
+
+    //死亡ステートの更新処理
+    void DieStateUpdate()
+    {
+
+    }
+
+
+    //死亡ステートの状態遷移処理
+    void DieChangeState()
+    {
+
     }
 }
