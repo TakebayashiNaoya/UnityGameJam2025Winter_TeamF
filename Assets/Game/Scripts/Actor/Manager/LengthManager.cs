a7f4a60662e26576b6b28075c8745241e49dc53c
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2025-12-16 12:12:56 +0900

    スクリプトのマネージャーフォルダを追加
    レングスマネージャーを追加
    未実装

---
+++ Assets/Game/Scripts/Actor/Manager/LengthManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CharacterType
+{
+    Player,
+    Enemy,
+    NumTypes
+}
+
+
+
+public class LengthManager : MonoBehaviour
+{
+    private const int playerMaxLength = 50;
+    private const int enemyMaxLength = 35;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
