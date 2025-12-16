3a5927d5b68f909bf1206e07483029891b4ddb6e
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2025-12-16 13:34:37 +0900

    プレイヤーとエネミーの仮のスクリプトを追加

--- Assets/Game/Scripts/Actor/Bases/Character/EnemyScript.cs
+++ Assets/Game/Scripts/Actor/Bases/Character/EnemyScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyScript : MonoBehaviour
+public class EnemyScript : CharacterBaseScript
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Game/Scripts/Actor/Bases/Character/PlayerScript.cs
+++ Assets/Game/Scripts/Actor/Bases/Character/PlayerScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour
+public class PlayerScript : CharacterBaseScript
 {
     // Start is called before the first frame update
     void Start()
