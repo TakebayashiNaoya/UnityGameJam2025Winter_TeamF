40e50be8931ca93c0a5524af5c7d2a0a0713aafc
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2025-12-17 12:28:46 +0900

    バトルマネージャーを作成

--- Assets/Game/Scripts/SoundManagerScript.cs
+++ Assets/Game/Scripts/SoundManagerScript.cs
@@ -8,11 +8,12 @@ public class SoundManager : MonoBehaviour
     // どこからでも呼び出せるようにする（シングルトン化）
     public static SoundManager instance;
 
-    [Header("BGM用オーディオソース")]
-    public AudioSource bgmAudioSource;
+    [Header("BGM用オーディオソース"), SerializeField]
+    private AudioSource bgmAudioSource;
 
-    [Header("SE用オーディオソース")]
-    public AudioSource seAudioSource;
+    [Header("SE用オーディオソース"), SerializeField]
+    private AudioSource seAudioSource;
+
 
     void Awake()
     {
@@ -29,7 +30,11 @@ public class SoundManager : MonoBehaviour
         }
     }
 
-    // BGMを再生する関数
+
+    /// <summary>
+    /// BGMを再生する関数
+    /// </summary>
+    /// <param name="clip"> 再生するBGMのAudioClip </param>
     public void PlayBgm(AudioClip clip)
     {
         // すでに同じ曲が流れているなら何もしない（リスタート防ぐため）
@@ -39,14 +44,21 @@ public class SoundManager : MonoBehaviour
         bgmAudioSource.Play();
     }
 
-    // BGMを止める関数
+
+    /// <summary>
+    /// BGMを停止する関数
+    /// </summary>
     public void StopBgm()
     {
         bgmAudioSource.Stop();
         bgmAudioSource.clip = null;
     }
 
-    // SEを再生する関数（ついでに実装しておきます）
+
+    /// <summary>
+    /// SEを再生する関数
+    /// </summary>
+    /// <param name="clip"> 再生するSEのAudioClip </param>
     public void PlaySe(AudioClip clip)
     {
         seAudioSource.PlayOneShot(clip);
