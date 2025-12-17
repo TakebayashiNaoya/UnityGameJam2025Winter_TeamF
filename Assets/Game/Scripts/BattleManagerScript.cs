3273a2abae15801eba9f742a80c3ebb4723a57f6
竹林 尚哉 <ca01254013@st.kawahara.ac.jp>
2025-12-17 13:49:57 +0900

    サウンドマネージャーとバトルマネージャーの実装 (#13)

---
+++ Assets/Game/Scripts/BattleManagerScript.cs
@@ -0,0 +1,63 @@
+﻿///
+/// 戦闘を管理するクラス
+///
+using UnityEngine;
+
+public class BattleManagerScript : MonoBehaviour
+{
+    [Header("戦闘BGM"), SerializeField] private AudioClip battleBgm;
+
+    [Header("勝利SE"), SerializeField] private AudioClip winFanfare;
+
+    [Header("敗北SE"), SerializeField] private AudioClip loseFanfare;
+
+    void Start()
+    {
+        SoundManager.instance.PlayBgm(battleBgm);
+    }
+
+    void Update()
+    {
+        // 戦闘終了の判定
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            GameClear();
+        }
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
+            GameOver();
+        }
+    }
+
+
+    /// <summary>
+    /// ゲームクリア時に呼ばれる関数
+    /// </summary>
+    private void GameClear()
+    {
+        // 流れている戦闘BGMを止める
+        SoundManager.instance.StopBgm();
+
+        // 勝利ファンファーレを「SE」として1回だけ鳴らす
+        if (winFanfare != null)
+        {
+            SoundManager.instance.PlaySe(winFanfare);
+        }
+    }
+
+
+    /// <summary>
+    /// ゲームオーバー時に呼ばれる関数
+    /// </summary>
+    private void GameOver()
+    {
+        // BGMを止める
+        SoundManager.instance.StopBgm();
+
+        // 敗北ファンファーレを鳴らす
+        if (loseFanfare != null)
+        {
+            SoundManager.instance.PlaySe(loseFanfare);
+        }
+    }
+}
---
+++ Assets/Game/Scripts/SoundManagerScript.cs
@@ -0,0 +1,66 @@
+﻿///
+/// サウンドを管理するクラス
+///
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    // どこからでも呼び出せるようにする（シングルトン化）
+    public static SoundManager instance;
+
+    [Header("BGM用オーディオソース"), SerializeField]
+    private AudioSource bgmAudioSource;
+
+    [Header("SE用オーディオソース"), SerializeField]
+    private AudioSource seAudioSource;
+
+
+    void Awake()
+    {
+        // これが存在しない場合、自分を代入
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject); // シーン遷移しても破壊されないようにする
+        }
+        else
+        {
+            // すでに存在する場合、重複しないように自分を破壊
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    /// <summary>
+    /// BGMを再生する関数
+    /// </summary>
+    /// <param name="clip"> 再生するBGMのAudioClip </param>
+    public void PlayBgm(AudioClip clip)
+    {
+        // すでに同じ曲が流れているなら何もしない（リスタート防ぐため）
+        if (bgmAudioSource.clip == clip) return;
+
+        bgmAudioSource.clip = clip;
+        bgmAudioSource.Play();
+    }
+
+
+    /// <summary>
+    /// BGMを停止する関数
+    /// </summary>
+    public void StopBgm()
+    {
+        bgmAudioSource.Stop();
+        bgmAudioSource.clip = null;
+    }
+
+
+    /// <summary>
+    /// SEを再生する関数
+    /// </summary>
+    /// <param name="clip"> 再生するSEのAudioClip </param>
+    public void PlaySe(AudioClip clip)
+    {
+        seAudioSource.PlayOneShot(clip);
+    }
+}
