06fd0c68d0082b39e785979f7491404585de04d4
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2025-12-18 18:31:30 +0900

    アクタークラスの名前変更、不要部分の削除

--- Assets/Game/Scripts/Actor/Actor.cs
+++ Assets/Game/Scripts/Actor/Actor.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//
+//アクタークラス
+//
 using UnityEngine;
 
-public abstract class ActorScript : MonoBehaviour
+public abstract class Actor : MonoBehaviour
 {
-    [Header("最大体力"), SerializeField] protected int maxHealth_;
+    [Header("最大体力"),         SerializeField] protected int maxHealth_;
     [Header("自身の当たり判定"), SerializeField] protected SphereCollider bodyCollider_;
 
 
