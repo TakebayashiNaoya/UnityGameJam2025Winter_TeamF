b49440b8691d3ef4aa9b6206d5ecdd8840459047
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2025-12-18 14:11:01 +0900

    拠点の体力スクリプトを追加

---
+++ Assets/Game/Scripts/BaseHealthScript.cs
@@ -0,0 +1,18 @@
+﻿///
+/// 拠点の体力を管理するスクリプト
+///
+using UnityEngine;
+
+public class BaseHealthScript : ActorScript
+{
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
