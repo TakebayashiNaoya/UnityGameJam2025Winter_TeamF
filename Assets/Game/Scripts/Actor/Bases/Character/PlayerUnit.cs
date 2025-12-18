0d0a110a1f8820a1c69bf4d33fbc14c8944ff25d
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2025-12-18 18:33:59 +0900

    自分のシーンを調整

--- Assets/Game/Scripts/Actor/Bases/Character/PlayerUnit.cs
+++ Assets/Game/Scripts/Actor/Bases/Character/PlayerUnit.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//
+//味方キャラクターのクラス
+//
 using UnityEngine;
 
-public class PlayerUnit : CharacterBaseScript
+public class PlayerUnit : CharacterBase
 {
     // Start is called before the first frame update
     void Start()
