725fa7bb450024c83e00e1dff0df2f7f3dbf8fd2
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2025-12-19 09:45:56 +0900

    スポナーのスクリプトを置き換え

---
+++ Assets/Game/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : Actor
+{
+    protected override void InitializeObject()
+    {
+        //体力を最大体力に設定
+        health_ = maxHealth_;
+    }
+
+    protected override void UpdateObject()
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeObject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
