1ea8191a1e23ebc650ca92c78806bf2184ed10fc
竹林 尚哉 <ca01254013@st.kawahara.ac.jp>
2025-12-19 11:07:14 +0900

    Takebayashi/create spawner (#31)

---
+++ Assets/Game/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Spawner : Actor
+{
+    [Header("体力表示のTMP"), SerializeField]
+    private TextMeshProUGUI hpText_;
+
+
+    protected override void InitializeObject()
+    {
+        //体力を最大体力に設定
+        health_ = maxHealth_;
+    }
+
+    protected override void UpdateObject()
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeObject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // HP表示
+        hpText_.text = health_.ToString() + " / " + maxHealth_.ToString();
+    }
+}
