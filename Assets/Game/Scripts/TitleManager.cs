8ebf2d3f9fd9aa1cd061b7e0814099de2f363489
竹林 尚哉 <ca01254013@st.kawahara.ac.jp>
2025-12-21 16:27:15 +0900

    Takebayashi/create title (#40)

---
+++ Assets/Game/Scripts/TitleManager.cs
@@ -0,0 +1,21 @@
+﻿///
+/// タイトルを管理するスクリプト
+///
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    [Header("タイトルBGM"), SerializeField] private AudioClip titleBgm_;          // 戦闘BGM
+
+    void Start()
+    {
+        // タイトルBGM再生
+        SoundManager.instance.PlayBgm(titleBgm_);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
