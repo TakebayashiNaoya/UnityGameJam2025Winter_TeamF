4d7325b5c8e85c044ebd3a25917e8ef5419b4567
TakebayashiNaoya <ca01254013@st.kawahara.ac.jp>
2025-12-21 16:16:44 +0900

    シーンを変更するスクリプトを追加

--- Assets/Game/Scripts/SceneTransition.cs
+++ Assets/Game/Scripts/SceneTransition.cs
@@ -1,19 +1,57 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement; // シーン管理に必要
+﻿///
+/// シーン遷移を管理するスクリプト
+///
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class SceneTransition : MonoBehaviour
 {
-    // ボタンから呼び出すためのパブリック関数
+    [SerializeField] private CanvasGroup fadeGroup_;        // で作ったCanvas Group
+    [SerializeField] private float fadeDuration_ = 0.5f;    // フェードにかける時
+
+
+    /// <summary>
+    /// 即時にシーンをロードする
+    /// </summary>
     public void LoadGameScene(string sceneName)
     {
-        // 指定した名前のシーンを読み込む
         SceneManager.LoadScene(sceneName);
     }
 
-    // 現在のシーンをリロード（やり直し）する場合
+
+    /// <summary>
+    /// 現在のシーンをリロードする
+    /// </summary>
     public void ReloadScene()
     {
         string currentSceneName = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene(currentSceneName);
+    }
+
+
+    /// <summary>
+    /// フェードアウトしてシーンをロードする
+    /// </summary>
+    public void FadeAndLoadScene(string sceneName)
+    {
+        StartCoroutine(FadeSequence(sceneName));
+    }
+    /// <summary>
+    /// フェードアウトシーケンス
+    /// </summary>
+    private IEnumerator FadeSequence(string sceneName)
+    {
+        float elapsed = 0;
+        fadeGroup_.blocksRaycasts = true;
+
+        while (elapsed < fadeDuration_)
+        {
+            elapsed += Time.deltaTime;
+            fadeGroup_.alpha = Mathf.Clamp01(elapsed / fadeDuration_);
+            yield return null;
+        }
+
+        SceneManager.LoadScene(sceneName);
     }
 }
